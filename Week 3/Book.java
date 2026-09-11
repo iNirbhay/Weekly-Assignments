3e277b246c1d33e0b64967ab0a7c367a368bd2bf
nk3754-code <nk3754@srmist.edu.in>
2026-09-11 21:46:31 +0530

    Add Week 3 OOP problems

---
+++ Week 3/Book.java
@@ -0,0 +1,12 @@
+public class Book {
+    String title;
+    double price;
+
+    public static void main(String[] args) {
+        Book book = new Book();
+        book.title = "Clean Code";
+        book.price = 650.0;
+
+        System.out.println("Title: " + book.title + " | Price: Rs " + book.price);
+    }
+}
